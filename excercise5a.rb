5a71a8cce35f0831097d501fa3b610a485985898
Sean Packham <sean@seanpackham.com>
2020-04-19 07:41:49 +0100

    Add Game and Maze class

--- excercise5a.rb
+++ excercise5a.rb
@@ -36,7 +36,7 @@ class Dynamic < Entity
     super(grid, x, y, char)
 
     # Power goes up as levels progress
-    @power = 20 * $level
+    @power = 20
   end
 
   def move(dx, dy)
@@ -100,75 +100,58 @@ class Enemy < Dynamic
 
   def initialize(grid, x, y)
     super(grid, x, y, "K")
-    @health = 100 * $level
+    @health = 100
   end
 end
 
+class Game
+  attr_accessor :level
+  attr_accessor :status
 
-$level = 1
-$status = "Let's get started"
+  def initialize
+    @level = 1
+    @status = "Let's get started"
 
-loop do
-  height = 20
-  width = 30
-  grid = []
-  for y in 0..height
-    # insert a new array for each row
-    # easier to read than the Array.new... stuff
-    grid.push([])
-    for x in 0..width
-      # outer wall
-      if x == 0 or y == 0 or x == width or y == height
-        Wall.new(grid, x, y)
-      end
+    @maze = Maze.new(@level)
+  end
+
+  def update
+    # Player input to move player
+    input = STDIN.getch.downcase
+
+    if input == "q"
+      exit 0
+    end
+
+    if input == "w"
+      @maze.player.move(0, -1)
+    elsif input == "s"
+      @maze.player.move(0, 1)
+    elsif input == "a"
+      @maze.player.move(-1, 0)
+    elsif input == "d"
+      @maze.player.move(1, 0)
+    else
+      @status = "Unknown action, nothing happens."
+    end
+
+    # Enemy movement
+    if @maze.enemy1.health <= 0
+      @level = @level + 1
+      @maze = Maze.new(@level)
+      @status = "Well done! You're now in level #{@level}"
+    else
+      enenmyMoveY = rand(-1..1)
+      enenmyMoveX = rand(-1..1)
+      @maze.enemy1.move(enenmyMoveY,enenmyMoveX)
     end
   end
 
-  #Create rooms that increase a level progresses
-  for time in 0..(4 * $level) do
-    a = rand(2..((width / 2)-2))
-    b = rand(2..((height / 2)-2))
-    c = (a * 2) + 3
-    d = (b * 2) + 3
-    for yy in b..d do
-      for xx in a..c do
-        if xx == c or yy == d or xx == a or yy == b
-          Wall.new(grid, xx, yy)
-        end
-       end
-     end
-  end
-
-  #Create a few doors in some rooms
-  for y in 1..(height - 1)
-    for x in 1..(width - 1)
-      # 10% chance to spawn a wall inside
-      if rand <= (0.1 + $level * 0.05)
-        grid[y][x] = nil
-      end
-    end
-  end
-
-  for z in 0..3 do
-    x = rand(1..(width - 1))
-    y = rand(1..(height - 1))
-    Weapon.new(grid, x, y)
-  end
-
-  x = rand(1..(width - 1))
-  y = rand(1..(height - 1))
-  z = rand(1..(width - 1))
-  w = rand(1..(height - 1))
-
-  enemy1 = Enemy.new(grid, x, y)
-  player = Player.new(grid, z, w)
-
-  loop do
-
+  def draw
     system "clear"
 
     # grid
-    for row in grid
+    for row in @maze.grid
       for column in row
         # floor
         if column.nil?
@@ -182,48 +165,84 @@ loop do
     end
 
     # Text summary below the grid
-    puts "Level: " + $level.to_s
-    puts "Health: #{player.health}"
-    puts "Power: #{player.power}"
-    puts "Enemy health: #{enemy1.health}"
-    puts "Status: " + $status
+    puts "Level: " + @level.to_s
+    puts "Health: #{@maze.player.health}"
+    puts "Power: #{@maze.player.power}"
+    puts "Enemy health: #{@maze.enemy1.health}"
+    puts "Status: " + @status
     print "WASD to move q to quit: "
+  end
+end
 
-    # Player input to move player
-    input = STDIN.getch.downcase
+class Maze
+  attr_accessor :grid
 
-    if input == "q"
-      exit 0
+  attr_reader :player, :enemy1
+
+  def initialize(level)
+    height = 20
+    width = 30
+
+    @grid = []
+
+    for y in 0..height
+      # insert a new array for each row
+      @grid.push([])
+
+      for x in 0..width
+        # outer wall
+        if x == 0 or y == 0 or x == width or y == height
+          Wall.new(@grid, x, y)
+        end
+      end
     end
 
-    if input == "w"
-      player.move(0, -1)
-    elsif input == "s"
-      player.move(0, 1)
-    elsif input == "a"
-      player.move(-1, 0)
-    elsif input == "d"
-      player.move(1, 0)
-    else
-      $status = "Unknown action, nothing happens."
+    #Create rooms that increase a level progresses
+    for time in 0..(4 * level) do
+      a = rand(2..((width / 2)-2))
+      b = rand(2..((height / 2)-2))
+      c = (a * 2) + 3
+      d = (b * 2) + 3
+
+      for yy in b..d do
+        for xx in a..c do
+          if xx == c or yy == d or xx == a or yy == b
+            Wall.new(@grid, xx, yy)
+          end
+         end
+       end
     end
 
-    # Enemy movement
-    if enemy1.health <= 0
-      $level = $level + 1
-      $status = "Well done! You're now in level #{$level}"
+    #Create a few doors in some rooms
+    for y in 1..(height - 1)
+      for x in 1..(width - 1)
+        # 10% chance to spawn a wall inside
+        if rand <= (0.1 + level * 0.05)
+          grid[y][x] = nil
+        end
+      end
+    end
 
-      # Move enemy when defeated in new level
-
+    for z in 0..3 do
       x = rand(1..(width - 1))
       y = rand(1..(height - 1))
-      enemy1 = Enemy.new(grid, x, y)
-      enemy1.health = 100
-      break
-    else
-      enenmyMoveY = rand(-1..1)
-      enenmyMoveX = rand(-1..1)
-      enemy1.move(enenmyMoveY,enenmyMoveX)
+      Weapon.new(@grid, x, y)
     end
+
+    x = rand(1..(width - 1))
+    y = rand(1..(height - 1))
+    z = rand(1..(width - 1))
+    w = rand(1..(height - 1))
+
+    @enemy1 = Enemy.new(@grid, x, y)
+    @player = Player.new(@grid, z, w)
   end
+end
+
+
+game = Game.new
+
+loop do
+  game.draw
+  game.update
 end
